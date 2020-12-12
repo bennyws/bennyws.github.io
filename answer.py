a8edb7e8a5306366d413f8cb19cf3b63783f3462
양 원석 <bennyws@naver.com>
2020-12-12 14:17:22 +0000

    #1181 단어 정렬 ver1

--- answer.py
+++ answer.py
@@ -1,16 +1,29 @@
 # answer.py
-# 11050 이항 계수 1
+# 1181 단어 정렬
 
-ans = 1
+L = []
+cL = []
 
-N, K = map(int, input().split())
+N = int(input())  # 단어의 개수
 
-if K > N-K:
-    K = N-K
+for i in range(N):  # 단어 입력
+    L.append(input())
 
-for i in range(N, N-K, -1):
-    ans *= i
-for j in range(K, 0, -1):
-    ans /= j
+L.sort()  # 리스트 정렬
 
-print(int(ans))
+for i in range(N):  # 중복 단어 제거
+    if L.count(L[i]) > 1:
+        for j in range(L.count(L[i]) - 1):
+            L.pop(L.index(L[i]))
+    if i + 1 >= len(L):  # 리스트 끝에 도달 시 종료
+        break
+
+for i in range(len(L)):  # L의 단어 길이를 담은 cL 생성
+    cL.append(len(L[i]))
+
+for i in range(len(L), 0, -1):  # 단어 길이순으로 정렬
+    n = int(cL.index(min(cL[:i])))
+    L.append(L.pop(n))
+    cL.append(cL.pop(n))
+
+print(L)

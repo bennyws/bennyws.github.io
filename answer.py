74ff91d72ccdf029a88528e0c59abff6353080df
양 원석 <bennyws@naver.com>
2020-12-12 10:44:04 +0000

    #11050 이항 계수 1

--- answer.py
+++ answer.py
@@ -1,18 +1,16 @@
 # answer.py
-# 2798 블랙잭
+# 11050 이항 계수 1
 
-n, m = map(int, input().split())  # n : 카드의 개수, m : 기준이 될 수
+ans = 1
 
-a = sorted(list(map(int, (input().split()))))  # 카드를 담을 리스트를 정렬해서 저장
+N, K = map(int, input().split())
 
-s = set()  # 합계를 담을 집합
+if K > N-K:
+    K = N-K
 
-for i in range(n-2):
-    for j in range(i+1, n-1):
-        for k in range(j+1, n):
-            csum = a[i] + a[j] + a[k]
-            if csum <= m:  # m 이하인 값만 추가
-                s.add(csum)
-                break
+for i in range(N, N-K, -1):
+    ans *= i
+for j in range(K, 0, -1):
+    ans /= j
 
-print(max(s))  # m 이하인 합들 중 최댓값 출력
+print(int(ans))

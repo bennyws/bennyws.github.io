b82ea89c03f20372e88c382002d799d278638000
양 원석 <bennyws@naver.com>
2020-12-12 06:47:24 +0000

    #1259  팰린드롬수

--- index.py
+++ index.py
@@ -1,27 +1,6 @@
 # index.py
 
-col, row = map(int, input().split())
-
-matrix = []
-for i in range(row):
-    matrix.append(list(input()))
-
-for i in range(col):
-    for j in range(row):
-        if matrix[i][j] == '.':
-            count = 0
-            for m in range(i-1, i+2):
-                if m < 0 or m >= col:
-                    continue
-                for n in range(j-1, j+2):
-                    if n < 0 or n >= row:
-                        continue
-                    if matrix[m][n] == '*':
-                        count += 1
-            matrix[i].pop(j)
-            matrix[i].insert(j, count)
-
-for i in range(col):
-    for j in matrix[i]:
-        print(j, end="")
-    print()
+while True:
+    a = input()
+    if a == '0': break
+    print('Yes' if a == a[::-1] else 'No')

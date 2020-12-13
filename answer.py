19193e9fbdc3ef449ec6691c4d7b1bb9a3a21168
양 원석 <bennyws@naver.com>
2020-12-13 10:27:19 +0000

    # 1920 수 찾기

--- answer.py
+++ answer.py
@@ -1,16 +1,38 @@
 # answer.py
-# 10814 나이순 정렬
+# 1920 수 찾기
 
-import sys
+
+def Binary_Search(L, N):
+    a = len(L)//2
+    if N > L[a]:
+        if(len(L) == 1):
+            return False
+        L = L[a+1:]
+        return Binary_Search(L, N)
+    elif N < L[a]:
+        if(len(L) == 1):
+            return False
+        L = L[:a]
+        return Binary_Search(L, N)
+    else:
+        return True
+
+
+a = [17, 28, 43, 67, 88, 92, 100]
+print(Binary_Search(a, 67))
+
+
+
+"""import sys
 input = sys.stdin.readline
 
-n = int(input())
-L = []
+input()
+A = set(map(int, input().split()))
+input()
+B = list(map(int, input().split()))
 
-for i in range(n):
-    a, b = map(int, input().split())
-    L.append((a, b))
-
-L = sorted(L, key=lambda x: (x[0], x[1]))
-for m, n in L:
-    print(m, n)
+for i in B:
+    if i in A:
+        sys.stdout.write('1\n')
+    else:
+        sys.stdout.write('0\n')"""

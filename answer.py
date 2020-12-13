21c4e51d949b5c798bc41f2523acfb99c66d3929
양 원석 <bennyws@naver.com>
2020-12-13 09:22:33 +0000

    # 10814 나이순 정렬

--- answer.py
+++ answer.py
@@ -1,13 +1,16 @@
 # answer.py
-# 2751 수 정렬하기 2
+# 10814 나이순 정렬
 
 from sys import stdin, stdout
 
 n = int(input())
-l = []
+L = []
 
 for i in range(n):
-    l.append(int(stdin.readline()))
+    a, b = stdin.readline().split()
+    L.append([int(a), b])
 
-for i in sorted(l):
-    stdout.write(str(i)+'\n')
+L = sorted(L, key=lambda x: [x[0]])
+
+for i, j in L:
+    stdout.write(str(i)+' '+j+'\n')

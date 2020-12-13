c0263dc2b5df310dac793bf5d02271de30b27c79
양 원석 <bennyws@naver.com>
2020-12-13 09:39:11 +0000

    # 11650 좌표 정렬하기

--- answer.py
+++ answer.py
@@ -1,16 +1,16 @@
 # answer.py
 # 10814 나이순 정렬
 
-from sys import stdin, stdout
+import sys
+input = sys.stdin.readline
 
 n = int(input())
 L = []
 
 for i in range(n):
-    a, b = stdin.readline().split()
-    L.append([int(a), b])
+    a, b = map(int, input().split())
+    L.append((a, b))
 
-L = sorted(L, key=lambda x: [x[0]])
-
-for i, j in L:
-    stdout.write(str(i)+' '+j+'\n')
+L = sorted(L, key=lambda x: (x[0], x[1]))
+for m, n in L:
+    print(m, n)

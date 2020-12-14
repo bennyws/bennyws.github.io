1b593bc73455b31f39403631670cef2a0d5e227e
양 원석 <bennyws@naver.com>
2020-12-14 14:18:26 +0000

    # 10816 숫자 카드 2 ver Counter

--- answer.py
+++ answer.py
@@ -6,24 +6,17 @@ import time
 start = time.time()
 
 import sys
+from collections import Counter
 input = sys.stdin.readline
 
 
-n = int(input())
-a = map(int, input().split())
-m = int(input())
-b = map(int, input().split())
-
-hashmap = {}
-
-for i in a:
-    if i in hashmap:
-        hashmap[i] += 1
-    else:
-        hashmap[i] = 1
-
-print(' '.join(str(hashmap[b]) if b in hashmap else '0' for b in b))
+_ = input()
+a = input().split()
+_ = input()
+b = input().split()
 
 
+c = Counter(a)
+print(' '.join(f'{c[m]}' if m in c else '0' for m in b))
 
 print("time :", time.time() - start)

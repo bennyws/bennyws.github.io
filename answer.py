ecb591035c663d8e65e5b45839be6396301c9d66
양 원석 <bennyws@naver.com>
2020-12-14 14:13:14 +0000

    # 10816 숫자 카드 2 ver해쉬 알고리즘

--- answer.py
+++ answer.py
@@ -1,33 +1,29 @@
 # answer.py
 # 10816 숫자 카드 2
 
-import sys
+
 import time
 start = time.time()
 
+import sys
 input = sys.stdin.readline
 
 
-def binary_search(L, n, low, high):
-    mid = (low + high) // 2
-    if low > high:
-        return 0
-
-    if L[mid] == n:
-        return L[low:high+1].count(n)
-    elif L[mid] > n:
-        return binary_search(L, n, low, mid - 1)
-    else:
-        return binary_search(L, n, mid + 1, high)
-
-
 n = int(input())
-a = sorted(list(map(int, input().split())))
+a = map(int, input().split())
 m = int(input())
-b = tuple(map(int, input().split()))
+b = map(int, input().split())
 
-for i in b:
-    tmp = binary_search(a, i, 0, n-1)
-    print(tmp, end=' ')
+hashmap = {}
+
+for i in a:
+    if i in hashmap:
+        hashmap[i] += 1
+    else:
+        hashmap[i] = 1
+
+print(' '.join(str(hashmap[b]) if b in hashmap else '0' for b in b))
+
+
 
 print("time :", time.time() - start)

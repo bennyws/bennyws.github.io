ac8ad8240b426a2835b31cd72ba9b4a18560f9c2
양 원석 <bennyws@naver.com>
2020-12-14 14:02:54 +0000

    # 10816 숫자 카드 2 ver이진탐색

--- answer.py
+++ answer.py
@@ -1,25 +1,33 @@
 # answer.py
-# 9012 괄호
+# 10816 숫자 카드 2
 
 import sys
+import time
+start = time.time()
 
 input = sys.stdin.readline
 
-n = int(input())
 
-for i in range(n):
-    a = input()
-    stack = 0
-    for j in range(len(a)):
-        if a[j] == '(':
-            stack += 1
-        elif a[j] == ')':
-            stack -= 1
-        else:
-            continue
-        if(stack < 0):
-            break
-    if stack == 0:
-        print('YES')
+def binary_search(L, n, low, high):
+    mid = (low + high) // 2
+    if low > high:
+        return 0
+
+    if L[mid] == n:
+        return L[low:high+1].count(n)
+    elif L[mid] > n:
+        return binary_search(L, n, low, mid - 1)
     else:
-        print('NO')
+        return binary_search(L, n, mid + 1, high)
+
+
+n = int(input())
+a = sorted(list(map(int, input().split())))
+m = int(input())
+b = tuple(map(int, input().split()))
+
+for i in b:
+    tmp = binary_search(a, i, 0, n-1)
+    print(tmp, end=' ')
+
+print("time :", time.time() - start)

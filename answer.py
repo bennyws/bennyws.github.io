21d6cf1135b0bb61312ffaaafb19f97215242cc8
양 원석 <bennyws@naver.com>
2020-12-21 11:16:59 +0000

    # 2630 색종이 만들기

--- answer.py
+++ answer.py
@@ -1,37 +1,46 @@
 # answer.py
-# 2606 바이러스
+# 2630 색종이 만들기
 
 # ---------------------------
 
 import sys
 input = sys.stdin.readline
 
+white = 0
+blue = 0
 
-def add(S, V, n):
-    for i in V[n]:
-        if i in S:
-            continue
-        else:
-            S.add(i)
-            add(S, V, i)
-    return
+
+def cut(x, y, n, L):
+    global white, blue
+    check = L[x][y]
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if check != L[i][j]:
+                cut(x, y, n//2, L)
+                cut(x, y + n//2, n//2, L)
+                cut(x + n//2, y, n//2, L)
+                cut(x + n//2, y + n//2, n//2, L)
+                return
+    if check == 0:
+        white += 1
+        return
+    else:
+        blue += 1
+        return
 
 
 def main():
+    global white, blue
     n = int(input())
-    m = int(input())
-    V = [[] for _ in range(n+1)]
-    arr = set()
-    arr.add(1)
+    a = []
 
-    for _ in range(m):
-        a, b = map(int, input().split())
-        V[a].append(b)
-        V[b].append(a)
+    for i in range(n):
+        a.append(list(map(int, input().split())))
 
-    add(arr, V, 1)
+    cut(0, 0, n, a)
+    print(white)
+    print(blue)
 
-    print(len(arr)-1)
 
 main()
 

fec4038cb4fba716baa3f07aa782f263c3961aab
양 원석 <bennyws@naver.com>
2020-12-22 10:49:34 +0000

    # 9095 1, 2, 3 더하기

--- answer.py
+++ answer.py
@@ -1,45 +1,23 @@
 # answer.py
-# 2630 색종이 만들기
+# 9095 1, 2, 3 더하기
 
 # ---------------------------
 
 import sys
 input = sys.stdin.readline
 
-white = 0
-blue = 0
-
-
-def cut(x, y, n, L):
-    global white, blue
-    check = L[x][y]
-    for i in range(x, x + n):
-        for j in range(y, y + n):
-            if check != L[i][j]:
-                cut(x, y, n//2, L)
-                cut(x, y + n//2, n//2, L)
-                cut(x + n//2, y, n//2, L)
-                cut(x + n//2, y + n//2, n//2, L)
-                return
-    if check == 0:
-        white += 1
-        return
-    else:
-        blue += 1
-        return
-
 
 def main():
-    global white, blue
+    dp = [1, 2, 4]
+
+    for i in range(3, 12):
+        dp.append(dp[i-3] + dp[i-2] + dp[i-1])
+
     n = int(input())
-    a = []
 
-    for i in range(n):
-        a.append(list(map(int, input().split())))
-
-    cut(0, 0, n, a)
-    print(white)
-    print(blue)
+    for _ in range(n):
+        num = int(input())
+        print(dp[num-1])
 
 
 main()

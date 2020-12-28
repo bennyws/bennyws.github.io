952357ec99bbbb9ddf28e87b00b42e9643c39430
양 원석 <bennyws@naver.com>
2020-12-28 07:54:55 +0000

    # 3-1 거스름돈

--- answer.py
+++ answer.py
@@ -1,5 +1,5 @@
 # answer.py
-# 11399 ATM
+# 3-1 거스름돈
 
 # ---------------------------
 
@@ -7,14 +7,14 @@ import sys
 input = sys.stdin.readline
 
 
-def main():
-    a = [1, 2]
+coin_list = [500, 100, 50, 10]
+count = 0
 
-    n = int(input())
-    for i in range(n-2):
-        a.append(sum(a[-2:]))
-    print(a[n-1] % 10007)
+n = int(input())
 
+for coin in coin_list:
+    count += n // coin
+    n %= coin
 
-main()
+print(count)
 

4507cd9df134645cea9616bd52dd80db4b21cc60
양 원석 <bennyws@naver.com>
2020-12-30 08:12:02 +0000

    # 4-3 왕실의 나이트

--- answer.py
+++ answer.py
@@ -1,18 +1,29 @@
 # answer.py
-# 4-2 시각
+# 4-3 왕실의 나이트
 
 # ---------------------------
 
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+
+a = list(input().rstrip())
+
+x = ord(a[0])-ord('a')+1
+y = int(a[1])
+
+dx = [1, -1, 1, -1, -2, -2, 2, 2]
+dy = [-2, -2, 2, 2, 1, -1, 1, -1]
+
 count = 0
 
-for hour in range(n+1):
-    if hour % 10 == 3:
-        count += 3600
-    else:
-        count += 1575  # 15 * 60 + (60-15) * 15  # 03 13 23 30~39 43 53
+for i in range(8):
+    nx = x + dx[i]
+    ny = y + dy[i]
+
+    if nx < 1 or ny < 1 or nx > 8 or ny > 8:
+        continue
+
+    count += 1
 
 print(count)

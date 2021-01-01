b3c0eb1cb9f95414fa28717f50767b9538eea165
양 원석 <bennyws@naver.com>
2021-01-01 14:15:20 +0000

    # 5-2 BFS 구현

--- answer.py
+++ answer.py
@@ -7,40 +7,36 @@ import sys
 input = sys.stdin.readline
 
 
-n, m = map(int, input().split())  # n = 세로크기 m = 가로크기
-a, b, d = map(int, input().split())  # a = 북쪽으로부터 거리 b = 서쪽으로부터 거리
-play_map = []
-count = 1
-direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # 북 동 남 서
+from collections import deque
 
-for _ in range(n):
-    play_map.append(list(map(int, input().split())))
 
-while True:
-    play_map[a][b] = 2
+def bfs(graph, start, visited):
+    queue = deque([start])
 
-    for i in range(4):
-        direct = (d + 3) % 4
-        na = a + direction[direct][0]
-        nb = b + direction[direct][1]
+    visited[start] = True
 
-        if na < 0 or nb < 0 or na >= n or nb >= m:
-            d = direct
-            continue
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
 
-        if play_map[na][nb] == 0:
-            a = na
-            b = nb
-            d = direct
-            count += 1
-            break
-        else:
-            d = direct
-            if i == 3:
-                a -= direction[d][0]
-                b -= direction[d][1]
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
 
-    if play_map[a][b] == 1:
-        break
 
-print(count)
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+bfs(graph, 1, visited)

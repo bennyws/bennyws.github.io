9526abbfa05bbb030567e851610f8bc0d7cc4757
양 원석 <bennyws@naver.com>
2021-01-03 13:39:45 +0000

    # 5-4 미로 탈출

--- answer.py
+++ answer.py
@@ -1,5 +1,5 @@
 # answer.py
-# 5-3 음료수 얼려 먹기
+# 5-4 미로 탈출
 
 # ---------------------------
 
@@ -7,38 +7,42 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-
-def bfs(ice, x, y):
-    global n, m
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    queue = deque([(x, y)])
-    ice[x][y] = 1
-
-    while queue:
-        a, b = queue.popleft()
-
-        for i in range(4):
-            nx = a + dx[i]
-            ny = b + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            elif ice[nx][ny] == 0:
-                queue.append((nx, ny))
-                ice[nx][ny] = 1
-
-
-n, m = map(int, input().split())  # n : 세로길이 m : 가로길이
-ice = []
+n, m = map(int, input().split())  # n = 세로길이 m = 가로길이
+gamemap = []
+visited = [[False] * m for _ in range(n)]
 count = 0
 
 for _ in range(n):
-    ice.append(list(map(int, input().rstrip())))
+    gamemap.append(list(map(int, input().rstrip())))
 
-for i in range(n):
-    for j in range(m):
-        if ice[i][j] == 0:
-            bfs(ice, i, j)
-            count += 1
 
+def bfs(gamemap, x, y, visited):
+    global count
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    queue = deque((x, y))
+    visited[x][y] = True
+
+    while queue:
+        a, b = queue.popleft()
+        print(a, b)
+        count += 1
+
+        for i in range(4):
+            tmp = 0
+            nx = a + dx[i]
+            ny = b + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            elif not visited[nx][ny] and gamemap[nx][ny] == 1:
+                visited[nx][ny] = True
+                queue.append([(nx, ny)])
+                tmp += 1
+
+            if tmp == 0:
+                count -= 1
+
+
+bfs(gamemap, 0, 0, visited)
 print(count)

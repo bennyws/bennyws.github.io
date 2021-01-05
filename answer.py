3564c46484d6bb22f0db589d9e71a08679c8f7ce
양 원석 <bennyws@naver.com>
2021-01-05 13:44:07 +0000

    # DFS/BFS 연습 - 1012 유기농 배추 ver.BFS

--- answer.py
+++ answer.py
@@ -1,33 +1,50 @@
 # answer.py
-# DFS/BFS 연습 - 2667 단지번호붙이기
+# DFS/BFS 연습 - 1012 유기농 배추
 
 # ---------------------------
 
+from collections import deque
 import sys
 input = sys.stdin.readline
 
 
-n = int(input())
-a = n
-L = []
+def bfs(field, x, y, visited):
+    visited[x][y] = True
+    queue = deque()
+    queue.append([x, y])
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            elif field[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                queue.append([nx, ny])
 
 
-def sum_of_num(num):
-    hap = 0
+T = int(input())  # T = 테스트 케이스의 개수
 
-    while num > 0:
-        hap += num % 10
-        num //= 10
+for _ in range(T):
+    m, n, k = map(int, input().split())  # m = 가로 n = 세로 k = 배추개수
+    field = [[0] * m for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]
+    count = 0
 
-    return hap
+    for _ in range(k):
+        a, b = map(int, input().split())
+        field[b][a] = 1
 
+    for i in range(n):
+        for j in range(m):
+            if field[i][j] == 1 and not visited[i][j]:
+                bfs(field, i, j, visited)
+                count += 1
 
-while a > 0:
-    a -= 1
-    if n == a + sum_of_num(a):
-        L.append(a)
-
-if len(L) == 0:
-    print(0)
-else:
-    print(min(L))
+    print(count)

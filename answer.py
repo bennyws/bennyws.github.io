14b3fb8c751ea95270d673c05ef5590a55234c38
양 원석 <bennyws@naver.com>
2021-01-09 08:18:20 +0000

    # 정렬 연습 - 1427 소트인사이드

--- answer.py
+++ answer.py
@@ -1,39 +1,19 @@
 # answer.py
-# 정렬 연습 - 2108 통계학
+# 정렬 연습 - 1427 소트인사이드
 
 # ---------------------------
 
-from collections import Counter
-import math
 import sys
 input = sys.stdin.readline
-
-
-def modefinder(array):
-    c = Counter(array)
-    order = c.most_common()
-    maximum = order[0][1]
-
-    modes = []
-    for num in order:
-        if num[1] == maximum:
-            modes.append(num[0])
-
-    if len(modes) > 1:
-        return modes[1]
-    else:
-        return modes[0]
-
 
 n = int(input())
 array = []
 
-for _ in range(n):
-    array.append(int(input()))
+while n > 0:
+    array.append(n % 10)
+    n //= 10
 
-array.sort()
+array.sort(reverse=True)
 
-print(round(sum(array)/n))
-print(array[math.floor(n/2)])
-print(modefinder(array))
-print(max(array)-min(array))
+for num in array:
+    print(num, end='')
